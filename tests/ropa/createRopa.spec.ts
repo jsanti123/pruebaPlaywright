704e0d750cb785d4e4fef4dd377b99a409619f67
jsanti123 <s.posada3@utp.edu.co>
2025-02-22 20:15:09 -0500

    Test  Create ROPA Successfully and Test Creation form Validation
    - Se finalizaron los test correespondientes a la creacion de un registro en el modulo de ROPA
    - Se adiciono un archi helper.ts, para algunas funciones genericas que sriven para realizar los test,

--- tests/ropa/createRopa.spec.ts
+++ tests/ropa/createRopa.spec.ts
@@ -3,6 +3,7 @@ import { LoginPage} from '../pages/loginPage';
 import { logInfo, logError } from '../utils/logger';
 import { BASE_URL, USERNAME, PASSWORD, VIEWPORT } from '../utils/constants';
 import { RopaPage } from '../pages/ropaPage';
+import { generateRandomString, generateRandomStatus, generateRandomResposiblePerson } from '../utils/helpers';
 
 // Cambia la resolución de la ventana
 test.use({
@@ -35,13 +36,27 @@ test.afterAll(async () => {
 
 test('Create ROPA successfully', async () => {
     try {
+        // Generate random data
+        const randomString = generateRandomString();
+        const randomStatus = generateRandomStatus();
+        const randomResponsiblePerson = generateRandomResposiblePerson();
+        // Go to Ropa
         await ropaPage.goToRopa();
         await expect(page).toHaveURL('https://pubivdjier-testing.priverion.dev/ropa');
         await expect(page.locator('span[data-cy="title-Record of Processing Activities"]')).toBeVisible();
         logInfo(`Accessing Ropa`);
+        // Create Ropa
         await page.getByRole('button', { name: 'Create' }).click();
         await page.locator('a[data-cy="menu-ropa-create"]').click();
-        await page.pause();
+        await page.locator('//div[@data-cy=\'select-type-responsible-person\']//input').fill(randomResponsiblePerson)
+        await page.keyboard.press('Enter');
+        await page.locator('//div[@data-cy=\'select-type-status\']//input').fill(randomStatus)
+        await page.keyboard.press('Enter');
+        await page.getByRole('textbox', { name: 'Name' }).fill(randomString);
+        await page.getByRole('button', { name: 'Save' }).click();
+        await expect(page).toHaveURL(/\/ropa\/detail\/.+/);
+        await expect(page.locator('div#tree-menu')).toBeVisible();
+        logInfo('Ropa created successfully');
     } catch (error) {
         logError(`Failed Test Create Ropa ${error}`);
     }
@@ -49,12 +64,21 @@ test('Create ROPA successfully', async () => {
 
 test('Creation form validation', async () => {
     try {
+        const randomStatus = generateRandomStatus();
+        const randomResponsiblePerson = generateRandomResposiblePerson();
         await ropaPage.goToRopa();
         await expect(page).toHaveURL('https://pubivdjier-testing.priverion.dev/ropa');
         await expect(page.locator('span[data-cy="title-Record of Processing Activities"]')).toBeVisible();
         logInfo(`Accessing Ropa`);
-        await page.pause();
-        
+        //Validar mensaje de error al no ingresar nombre
+        await page.getByRole('button', { name: 'Create' }).click();
+        await page.locator('a[data-cy="menu-ropa-create"]').click();
+        await page.locator('//div[@data-cy=\'select-type-responsible-person\']//input').fill(randomResponsiblePerson);
+        await page.keyboard.press('Enter');
+        await page.locator('//div[@data-cy=\'select-type-status\']//input').fill(randomStatus);
+        await page.keyboard.press('Enter');
+        await page.getByRole('button', { name: 'Save' }).click();
+        await expect(page.locator('span[data-cy="error-field_required"]')).toBeVisible();
     } catch (error) {
         logError(`Failed Test Create Ropa ${error}`);
     }
---
+++ tests/utils/helpers.ts
@@ -0,0 +1,19 @@
+export function generateRandomString(length: number = 20): string {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+  }
+
+export function generateRandomStatus(): string {
+    const status = ['Draft', 'Active', 'Review', 'Inactive'];
+    return status[Math.floor(Math.random() * status.length)];
+  }
+
+export function generateRandomResposiblePerson(): string {
+    const status = ['Admin', 'Sanitago Posada Florez', 'Marcelo'];
+    return status[Math.floor(Math.random() * status.length)];
+  }
+  
